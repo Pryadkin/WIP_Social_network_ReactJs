f9aa3ec46aac4d30f9b586968727d11aff78436d
Pryadkin Anton <pryadkin.a@citilink.ru>
2019-12-09 20:22:29 +0300

    add friends

--- src/redux/state.js
+++ src/redux/state.js
@@ -21,9 +21,12 @@ const state = {
    },
    sitebar: {
       friends: [
-         {name: 'Andrew', img: 'https://image.flaticon.com/icons/svg/1100/1100023.svg'},
-         {name: 'Sasha', img: 'https://image.flaticon.com/icons/svg/1100/1100025.svg'},
-         {name: 'Sveta', img: 'https://image.flaticon.com/icons/svg/1100/1100049.svg'},
+         {name: 'Artem', img: 'https://image.flaticon.com/icons/svg/1100/1100023.svg'},
+         {name: 'Dima', img: 'https://image.flaticon.com/icons/svg/1100/1100025.svg'},
+         {name: 'Nadya', img: 'https://image.flaticon.com/icons/svg/1100/1100049.svg'},
+         {name: 'Vadim', img: 'https://image.flaticon.com/icons/svg/1100/1100024.svg'},
+         {name: 'Olya', img: 'https://image.flaticon.com/icons/svg/1100/1100035.svg'},
+         {name: 'Aleksandr', img: 'https://image.flaticon.com/icons/svg/1100/1100019.svg'},
       ]
    }
 }
