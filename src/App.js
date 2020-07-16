033304ae7da1833e79162c6270ac3beda2776d6c
Anton Pryadkin <zorg200687@gmail.com>
2020-07-16 22:08:04 +0300

    split imports of App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
+// components
 import Header from './components/Header/Header'
 import Profile from './components/Profile/Profile'
 import SitebarContainer from './components/Sitebar/SitebarContainer'
 import DialogsContainer from './components/Dialogs/DialogsContainer'
-import { Route } from 'react-router-dom'
+
+// styles
 import './App.css'
 
-
 function App() {
-  return (    
-    <div className="app-wrapper">      
+  return (
+    <div className="app-wrapper">
       <Header />
       <SitebarContainer />
-      <Route path='/profile' render={ () => (
-          <Profile />
-        )
-      }/>
-      <Route path='/dialog' render={ () => (
-          <DialogsContainer />
-        )
-      }/>
-      
+      <Route path='/profile' render={() => (
+        <Profile />
+      )
+      } />
+      <Route path='/dialog' render={() => (
+        <DialogsContainer />
+      )
+      } />
+
     </div>
-    
+
   );
 }
 
